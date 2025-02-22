a7b54ca193f918d8dfe03bc339aa37e809a9ec4d
Oneil Achord <oneil.achord22@my.northampton.ac.uk>
2025-02-22 02:29:14 +0000

    more styling for home page

--- Website Frontend/exporhub-app/src/app/home/components/LatestProjectUpdates.tsx
+++ Website Frontend/exporhub-app/src/app/home/components/LatestProjectUpdates.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 export default function LatestProjectUpdates() {
     return (
         <aside className='sticky top-4'>
-            <div className='min-h-[36rem] size-full rounded-r-3xl p-4 bg-gradient-to-b from-pink-100 to-pink-500'>
+            <div className='min-h-[40rem] size-full rounded-r-3xl p-4 bg-gradient-to-b from-pink-100 to-pink-500 shadow-lg shadow-pink-300'>
+                <div className='text-2xl font-bold'>Latest Project Updates</div>
+                <div>
 
+                </div>
             </div>
         </aside>
     )
--- Website Frontend/exporhub-app/src/app/home/page.tsx
+++ Website Frontend/exporhub-app/src/app/home/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import NavBarComponent from '../components/NavBarComponent'
 import FooterComponent from '../components/FooterComponent'
 import LatestProjectUpdates from './components/LatestProjectUpdates'
+import Image from 'next/image'
+import ArrowSVG from '../../../public/right-arrow-white.svg'
 
 export default function Home() {
   return (
@@ -12,36 +14,19 @@ export default function Home() {
           <LatestProjectUpdates />
         </div>
         <main className='flex-[3_1_0%]'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem repellat qui necessitatibus corrupti? Harum voluptatibus dolorum aliquam dignissimos, necessitatibus rerum quod commodi est quos, iste ullam autem quam ab beatae!
-          Vel inventore, illo nesciunt, enim recusandae odit perferendis nisi sequi dolore eveniet ex optio nulla assumenda ad. Porro voluptates, quae quidem beatae repudiandae minima vel non veritatis odio consectetur eos.
-          Iste magni fugiat eaque consequatur reiciendis qui quam in! Earum totam aliquid animi repudiandae beatae eius non optio sequi minus consequatur numquam labore explicabo iusto quam provident distinctio, dolorem sed?
-          Incidunt eius ab ex expedita nihil sint, est inventore, doloribus alias voluptatem esse! Accusamus quidem qui dolor accusantium architecto sapiente iste eligendi, harum tenetur rerum neque numquam reprehenderit, cupiditate aperiam!
-          Nulla dolorem optio officiis soluta corporis fugiat, inventore nemo! Expedita cupiditate a corrupti! Error, cumque quasi omnis et tempora perferendis pariatur magnam quaerat quos ipsam architecto beatae fugit, autem aspernatur!
-          Reiciendis dolor quae ea, ad ducimus amet, voluptas fugiat autem sint quaerat ab in inventore expedita voluptates! Veritatis cum reprehenderit sed maxime incidunt similique, et, fugit, nesciunt ab veniam tempore.
-          Veniam dolores fuga amet nostrum enim, vel laudantium omnis voluptate, id ullam asperiores numquam tempora molestiae animi et eos recusandae nam ex adipisci earum placeat. A, similique. Reiciendis, tempore! Inventore.
-          Nihil accusamus perspiciatis impedit, minima quam quidem. Natus quidem est inventore, blanditiis libero fugiat aperiam numquam aspernatur alias illo ipsum quasi aliquam iste ab quod itaque tenetur. Sit, libero ducimus.
-          Facilis ad illum repellat inventore itaque expedita porro consectetur minima quae. Fuga dolor magnam rem quo cum, veritatis ab iste minus in quas perferendis amet voluptatem molestiae numquam! Ab, eligendi!
-          Reiciendis eaque, quaerat maxime tenetur ratione consectetur illo eligendi. Vitae molestiae unde ullam ratione. Suscipit voluptatibus officiis sint doloribus dignissimos, voluptate ea error quidem quaerat similique accusantium nihil, facere modi?
-          Nisi quibusdam dignissimos maxime nostrum blanditiis amet sapiente neque labore itaque pariatur ut iste qui laborum, magnam exercitationem ducimus natus aut nemo unde? Modi quidem nulla, quisquam similique tenetur aut.
-          Corrupti voluptatum similique harum autem explicabo accusantium magni quis commodi nostrum sunt illo placeat pariatur consequatur maiores, incidunt molestias eos? Sed qui voluptate minima doloremque omnis ducimus ullam iusto quos?
-          Non sunt fugiat consequuntur sit eius officia accusamus, enim inventore nesciunt? Exercitationem eius aut dicta porro est dolores dolor? Nesciunt earum consectetur eius illo natus nobis aut iste voluptates autem!
-          Voluptatem mollitia nisi optio, voluptates natus nam nobis quo error delectus molestias iure corporis magnam quidem saepe dolore? Delectus nesciunt porro dolorum dolor voluptatum. Omnis eaque consectetur laudantium quidem iure.
-          Corrupti suscipit rem, quisquam similique recusandae vero. Minus qui, enim at eaque cupiditate autem a commodi ipsa quasi dolore alias laudantium excepturi quod, facilis aliquam delectus tempora quae aut dolorum.
-          Porro, maiores, voluptate quo magni voluptatum rerum consectetur ut voluptatem labore magnam deserunt amet. Dolores harum libero atque debitis nisi, autem magnam quo, doloribus ducimus sequi nihil culpa minus laboriosam.
-          Ab repellat ullam quaerat iure vitae delectus mollitia recusandae praesentium facere molestiae aspernatur aut, doloremque et reprehenderit perferendis ipsum repudiandae autem suscipit natus, sunt nostrum deleniti? Sed sit impedit dolor!
-          Asperiores nobis nulla mollitia sapiente fuga excepturi recusandae ipsum, rerum officiis voluptatem et libero, minus consectetur iste corporis illum minima praesentium fugiat culpa dolorem, impedit accusantium tempore itaque esse. Libero!
-          Consequuntur reiciendis, voluptatibus aliquam suscipit quidem nihil, sit unde enim molestias ullam nobis fugiat, a libero laudantium ipsam nostrum. Distinctio sed earum maiores quae quia illo tempora nostrum porro fugit!
-          Porro expedita fugit deserunt sit ex sequi quis suscipit, ipsum itaque. Repellendus incidunt minima cumque, vel natus ipsam quod laborum, autem, iste eligendi dolore animi consequuntur aut officia vitae accusantium!
-          Et iure deserunt iusto! Debitis ab maxime tempore dolore earum? Impedit ad deleniti provident numquam nostrum pariatur qui eveniet libero nulla! Possimus error earum, alias voluptas esse numquam libero dolor.
-          Officiis sint, illum deserunt dolore deleniti recusandae repudiandae quibusdam quis repellat qui quo cupiditate, dolorum quas vel placeat aperiam nostrum et nesciunt exercitationem aut libero vero! Dolores cupiditate repellat accusamus?
-          Vel laudantium quidem nobis cum rerum veritatis repellendus optio, mollitia, id eos tenetur nulla. Nesciunt qui tempora reprehenderit nisi eius veritatis itaque voluptatem, odit exercitationem velit quia! Tempore, eum fugit.
-          Magnam ut enim amet vitae quam nam nulla, ab, sapiente, quaerat vel quis nisi molestiae repellat beatae eveniet nostrum et magni voluptatum delectus itaque officia optio blanditiis! Officiis, quis vel.
-          Reprehenderit aperiam nulla molestiae nobis quia temporibus pariatur velit praesentium ut ullam excepturi et aliquam in, sequi blanditiis aut fugit! Quidem asperiores ab provident aliquam tenetur aut fugiat nesciunt impedit!
-          Dolores deserunt, quaerat fugit impedit deleniti mollitia quam. Excepturi a aspernatur dignissimos, officia porro nihil. Quam dolorem minus voluptas dolor atque rem voluptatem. Nobis quasi in aspernatur ratione placeat ut.
-          Libero deserunt fuga blanditiis inventore ut magni reiciendis laudantium sunt numquam magnam similique, eligendi, placeat aperiam accusamus officia alias natus et veritatis ratione architecto eius in dolorem corporis voluptatibus? Rerum?
-          Magnam voluptas totam adipisci. Pariatur sit iusto nisi corrupti nemo ullam veniam, accusantium repellendus labore nostrum error impedit nobis voluptatum fugiat sequi enim libero minima exercitationem recusandae? Omnis, distinctio dolorem.
-          Hic esse facilis dignissimos illo eligendi inventore earum corrupti assumenda magnam quo minus repellat nam fugit rerum minima quia, cupiditate explicabo architecto perspiciatis? Nihil voluptatem, exercitationem distinctio quasi eveniet eos!
-          Sequi veritatis voluptas accusamus voluptatibus provident eos! Consequatur reiciendis asperiores molestiae fugit. Voluptate, sint. Officia laboriosam itaque dolores explicabo. Atque nostrum maxime temporibus suscipit cumque assumenda nemo, a est doloribus.
+          <div className='mt-48 ml-12 m-4'>
+            <div className='flex items-center gap-8'>
+              <div className='text-xl'>Trending Projects</div>
+              <div className='flex justify-center size-8 rounded-full bg-black'>
+                <button>
+                <Image src={ArrowSVG} title='Filter options' width={24} alt='Arrow image'/>
+                </button>
+              </div>
+            </div>
+            <div>
+
+            </div>
+          </div>
         </main>
       </section>
       <FooterComponent />
