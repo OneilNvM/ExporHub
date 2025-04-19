05b92a95d3a9d6bf5527f9ca1f6e4812e39391b9
Oneil Achord <oneil.achord22@my.northampton.ac.uk>
2025-04-19 23:06:39 +0100

    reconfigured docker to use Octane over PHP-FPM, nginx server optimizations, compose.yaml entrypoint command and exposed port changed to 8000, Dockerfile changes, added login route, changed artillery test file with the latest configuration for the load testing.

--- php-implementation/exporhub-backend/routes/api.php
+++ php-implementation/exporhub-backend/routes/api.php
@@ -5,14 +5,12 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get(
+    '/',
+    fn(Request $request) => $request->user
+);
 
-Route::get('/', fn(Request $request) => $request->user
-)->middleware('auth:sanctum');
-
-Route::get('/all', function() {
+Route::get('/all', function () {
     $users = DB::table('users')->get();
     $projects = DB::table('projects')->get();
     $images = DB::table('images')->get();
@@ -48,115 +46,108 @@ Route::get('/all', function() {
     return $results;
 });
 
-Route::get('/users', function() {
+Route::get('/users', function () {
     $users = DB::table('users')->get();
 
     return $users;
 });
 
-Route::get('/projects', function() {
+Route::get('/projects', function () {
     $projects = DB::table('projects')->get();
 
     return $projects;
 });
 
-Route::get('/images', function() {
+Route::get('/images', function () {
     $images = DB::table('images')->get();
 
     return $images;
 });
 
-Route::get('/follows', function() {
+Route::get('/follows', function () {
     $follows = DB::table('follows')->get();
 
     return $follows;
 });
 
-Route::get('/favourites', function() {
+Route::get('/favourites', function () {
     $favourites = DB::table('favourites')->get();
 
     return $favourites;
 });
 
-Route::get('/comments', function() {
+Route::get('/comments', function () {
     $comments = DB::table('comments')->get();
 
     return $comments;
 });
 
-Route::get('/replies', function() {
+Route::get('/replies', function () {
     $replies = DB::table('replies')->get();
 
     return $replies;
 });
 
-Route::get('/threads', function() {
+Route::get('/threads', function () {
     $threads = DB::table('threads')->get();
 
     return $threads;
 });
 
-Route::get('/likes', function() {
+Route::get('/likes', function () {
     $likes = DB::table('likes')->get();
 
     return $likes;
 });
 
-Route::get('/dislikes', function() {
+Route::get('/dislikes', function () {
     $dislikes = DB::table('dislikes')->get();
 
     return $dislikes;
 });
 
-Route::get('/comment-likes', function() {
+Route::get('/comment-likes', function () {
     $comment_likes = DB::table('comment_likes')->get();
 
     return $comment_likes;
 });
 
-Route::get('/comment-dislikes', function() {
+Route::get('/comment-dislikes', function () {
     $comment_dislikes = DB::table('comment_dislikes')->get();
 
     return $comment_dislikes;
 });
 
-Route::get('/reply-likes', function() {
+Route::get('/reply-likes', function () {
     $reply_likes = DB::table('reply_likes')->get();
 
     return $reply_likes;
 });
 
-Route::get('/reply-dislikes', function() {
+Route::get('/reply-dislikes', function () {
     $reply_dislikes = DB::table('reply_dislikes')->get();
 
     return $reply_dislikes;
 });
 
-Route::post('/login', function(Request $request) {
+Route::match(['post', 'options'], '/signin', function (Request $request) {
     $identity = $request->input('username_or_email');
 
-    // if (str_contains($identity, "@")) {
-    //     $user = DB::table('users')->where('username', $identity)->firstOrFail();
+    if (str_contains($identity, "@")) {
+        $user = DB::table('users')->where('email', $identity)->firstOrFail();
 
-    //     if ($user) {
-    //         return 'Invalid credentials';
-    //     } else {
-    //         $password = Hash::check($request->input('password'), $user->password);
-            
-    //         return $password ? $user : 'Invalid credentials';    
-    //     }
-    // } else {
-    //     $user = DB::table('users')->where('email', $identity)->firstOrFail();
+        $password = Hash::check($request->input('password'), $user->password);
 
-    //     if ($user->isEmpty()) {
-    //         return 'Invalid credentials';
-    //     } else {
-    //         $password = Hash::check($request->input('password'), $user->password);
+        return $password ? $user : response('Invalid credentials');
+    } else {
+        $user = DB::table('users')->where('username', $identity)->firstOrFail();
 
-    //         return $password ? $user : 'Invalid credentials';  
-    //     }
-    // }
+        $password = Hash::check($request->input('password'), $user->password);
 
-    return $identity;
+        return $password ? $user : response('Invalid credentials');
+    }
 });
 
+Route::post('/test', function () {
+    return response()->json(['message' => 'API test route hit!']);
+});
